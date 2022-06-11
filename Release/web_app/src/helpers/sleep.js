3801d98fe0d3da8ab0b85ceba6c0ae9c11937fd6
hungt1 <nguyenvanhung.brvt.2002@gmail.com>
2022-06-11 11:14:33 +0700

    update

--- Release/web_app/src/helpers/sleep.js
+++
@@ -1,7 +0,0 @@
-function timeout(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-export async function sleep(fn, ms, ...args) {
-  await timeout(ms);
-  return fn(...args);
-}
